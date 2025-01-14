28cefce410c110b0447c86e660e13c2970177d17
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-01-14 15:33:35 +0530

    :zap: :art: traffic light structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
+const trafficLight = [
+  {
+    color: "red",
+    backgroundClass: "background-red",
+    wait: 3000,
+  },
+  {
+    color: "yellow",
+    backgroundClass: "background-yellow",
+    wait: 1000,
+  },
+  {
+    color: "green",
+    backgroundClass: "background-green",
+    wait: 1000,
+  },
+];
+
 function App() {
-  return <></>;
+  const [activeLight, setActiveLight] = useState("red");
+
+  return (
+    <div className="App">
+      <h1>Traffic Light</h1>
+      <div className="trafficLightContainer">
+        {trafficLight.map((light, index) => {
+          return (
+            <div
+              key={index}
+              className={`trafficLight ${
+                activeLight === light.color
+                  ? light.backgroundClass
+                  : "background-gray"
+              }`}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
